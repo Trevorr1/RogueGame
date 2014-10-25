aefdf79bbfce0ce1c4a861c3beaf90513e409b80
Huan Nguyen <Huan500@hotmail.com>
2014-10-25 23:58:11 +0200

    Minor fix

--- Rogue/Rogue/Main.cpp
+++ Rogue/Rogue/Main.cpp
@@ -12,7 +12,7 @@ using namespace std;
 
 int main(int argc, const char* argv[])
 {
-	/*printf("\nHello World\n\n");
+	/*printf("\nHello World\n\n"); */
 
 	Manager*  manager = new Manager();
 	MenuRoom* menuRoom = new MenuRoom();
--- Rogue/Rogue/Manager.h
+++ Rogue/Rogue/Manager.h
@@ -1,7 +1,6 @@
 #pragma once
 #include <stdio.h>
 #include <iostream>
-#include <iostream>
 #include <fstream>
 #include <string>
 
