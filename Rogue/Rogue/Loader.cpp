a07ba81d242f0faee8be0dd09c8e32fee75c85cd
Huan Nguyen <Huan500@hotmail.com>
2014-11-05 22:42:47 +0100

    Load method loadRoomTraits

--- Rogue/Rogue/Loader.cpp
+++ Rogue/Rogue/Loader.cpp
@@ -4,13 +4,21 @@
 Loader::Loader()
 {
 	// rooms trait init
-	loadRoomTraits();
 	vectorRoomKinds.push_back("room_size");
 	vectorRoomKinds.push_back("room_state");
 	vectorRoomKinds.push_back("room_illumation");
 	vectorRoomKinds.push_back("room_shape");
 	vectorRoomKinds.push_back("room_content");
 	vectorRoomKinds.push_back("room_special_trait");
+
+	room_sizes = new vector<string>;
+	room_states = new vector<string>;
+	room_lightings = new vector<string>;
+	room_shapes = new vector<string>;
+	room_contents = new vector<string>;
+	room_specialTraits = new vector<string>;
+
+	loadRoomTraits();
 
 }
 
@@ -19,45 +27,6 @@ Loader::~Loader()
 {
 }
 
-void Loader::loadRoomTraits()
-{
-	string textFile("Rooms.txt");
-
-	ifstream input_file(textFile); 
-	string line;
-
-	int i = 0;
-
-
-	while (getline(input_file, line)) 
-	{
-		if (line.find("room_size") == std::string::npos)
-		{
-			room_sizes.push_back(line);
-		}
-		else if (line.find("room_state") == std::string::npos)
-		{
-			room_states.push_back(line);
-		}
-		else if (line.find("room_illumation") == std::string::npos)
-		{
-			room_shapes.push_back(line);
-		}
-		else if (line.find("room_shape") == std::string::npos)
-		{
-			room_lightings.push_back(line);
-		}
-		else if (line.find("room_content") == std::string::npos)
-		{
-			room_contents.push_back(line);
-		}
-		else if (line.find("room_special_trait") == std::string::npos)
-		{
-			room_specialTraits.push_back(line);
-		}
-		cout << "Loading Room Traits" << endl;
-	}
-}
 void Loader::loadEnemyList()
 {
 	string textfile("EnemyList.txt");
@@ -70,58 +39,48 @@ void Loader::loadItemList()
 	cout << "Loading Room Traits" << endl;
 }
 
-Room* Loader::loadFileRandomRoom(){
+void Loader::loadRoomTraits(){
 	string textFileClass = "Rooms.txt";
 
 	// (2a) Tekst inlezen uit een file met de klasse std::ifstream (input file stream)
 	ifstream input_file(textFileClass); // stack-based file object; deze constructie opent de file voor lezen
 	string line;
 
-	vector <string>* vectorRoomKinds = new vector<string>;
-	vectorRoomKinds->push_back("room_size");
-	vectorRoomKinds->push_back("room_state");
-	vectorRoomKinds->push_back("room_illumation");
-	vectorRoomKinds->push_back("room_shape");
-	vectorRoomKinds->push_back("room_content");
-	vectorRoomKinds->push_back("room_special_trait");
-
-	vector <string>* vectorLoaded = new vector<string>;
-	vector <string>* vectorRandomized = new vector<string>;
-	Room* room = new Room();
 	int i = 0;
 	//srand(time(0));
 
-	while (getline(input_file, line)) { // getline() geeft false zodra end-of-file is bereikt
-		//cout << line << '\n'; // getline() haalt de \n wel uit de stream, maar voegt die niet toe
+	while (getline(input_file, line)) {
 
 		//if line contains == true
-		if (line.find(vectorRoomKinds->at(i)) != std::string::npos){
-			if (i != 0){
-				int randItem = rand() % vectorLoaded->size();
-				vectorRandomized->push_back(vectorLoaded->at(randItem));
-
-				delete vectorLoaded;
-				vectorLoaded = new vector<string>;
-			}
-			if (i < vectorRoomKinds->size() - 1){
-				i++;
-			}
+		if (i < vectorRoomKinds.size() && line.find(vectorRoomKinds.at(i)) != std::string::npos){
+			i++;
 		}
 		else {
 			//cout << "kinds"<< endl;
-			vectorLoaded->push_back(line);
-		}
+			if (vectorRoomKinds.at(i-1) == "room_size"){
+				room_sizes->push_back(line);
+			}
+			else if (vectorRoomKinds.at(i-1) == "room_state"){
+				room_states->push_back(line);
+			}
+			else if (vectorRoomKinds.at(i-1) == "room_illumation")
+			{
+				room_lightings->push_back(line);
+			}
+			else if (vectorRoomKinds.at(i-1) == "room_shape")
+			{
+				room_shapes->push_back(line);
+			}
+			else if (vectorRoomKinds.at(i-1) == "room_content")
+			{
+				room_contents->push_back(line);
+			}
+			else if (vectorRoomKinds.at(i-1) == "room_special_trait")
+			{
+				room_specialTraits->push_back(line);
+			}
 
-		if (input_file.eof()){
-			int randItem = rand() % vectorLoaded->size();
-			vectorRandomized->push_back(vectorLoaded->at(randItem));
 		}
 	}
-
-	room->setRoom(vectorRandomized);
-
-	delete vectorRoomKinds;
-	delete vectorLoaded;
-	return room;
 
 }
--- Rogue/Rogue/Loader.h
+++ Rogue/Rogue/Loader.h
@@ -18,16 +18,14 @@ public:
 	void loadEnemyList();
 	void loadItemList();
 
-	Room* loadFileRandomRoom();
-
 private:
 	vector <string> vectorRoomKinds;
-	vector<string> room_sizes;
-	vector<string> room_states;
-	vector<string> room_shapes;
-	vector<string> room_lightings;
-	vector<string> room_contents;
-	vector<string> room_specialTraits;
+	vector<string>* room_sizes;
+	vector<string>* room_states;
+	vector<string>* room_lightings;
+	vector<string>* room_shapes;
+	vector<string>* room_contents;
+	vector<string>* room_specialTraits;
 
 	vector<string> enemies;
 
